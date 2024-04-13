58494e14172e1664ea5d217fde799edc3f364307
Artem_Ziablov <113183341+proge6@users.noreply.github.com>
2024-04-13 19:58:29 +0200

    add functional for deleting book copy and added logger from slf4j

--- src/main/java/ua/karazin/interfaces/ProjectLibrary/services/BookCopyService.java
+++ src/main/java/ua/karazin/interfaces/ProjectLibrary/services/BookCopyService.java
@@ -3,9 +3,13 @@ package ua.karazin.interfaces.ProjectLibrary.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ua.karazin.interfaces.ProjectLibrary.exceptions.BookCopyNotFoundException;
+import ua.karazin.interfaces.ProjectLibrary.exceptions.BookNotReturnedFromReaderException;
 import ua.karazin.interfaces.ProjectLibrary.models.BookCopy;
 import ua.karazin.interfaces.ProjectLibrary.repositories.BookCopyRepo;
 import ua.karazin.interfaces.ProjectLibrary.exceptions.BookNotRegisteredException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +25,22 @@ public class BookCopyService {
             newBookCopy.setStatus(bookCopy.getStatus());
 
             bookCopyRepo.save(newBookCopy);
+        }
+    }
+
+    Optional<BookCopy> findBookCopyByCopyId(int copyId){
+        return bookCopyRepo.findByCopyId(copyId);
+    }
+
+    @Transactional
+    public void deleteBookCopy(int copyId) {
+        var bookCopyToDelete = findBookCopyByCopyId(copyId);
+        if (bookCopyToDelete.isEmpty()){
+            throw new BookCopyNotFoundException();
+        } else if (bookCopyToDelete.get().getReader().isPresent()){
+            throw new BookNotReturnedFromReaderException();
+        } else {
+            bookCopyRepo.delete(bookCopyToDelete.get());
         }
     }
 }
