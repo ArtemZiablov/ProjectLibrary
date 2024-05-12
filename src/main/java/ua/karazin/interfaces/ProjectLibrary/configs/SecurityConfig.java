9f4a56fbdfc47ae7932495035f6b16d0999f9419
Artem_Ziablov <artem.ziablov13@gmail.com>
2024-05-12 15:27:20 +0200

    jwt v1

--- src/main/java/ua/karazin/interfaces/ProjectLibrary/configs/SecurityConfig.java
+++ src/main/java/ua/karazin/interfaces/ProjectLibrary/configs/SecurityConfig.java
@@ -1,16 +1,23 @@
 package ua.karazin.interfaces.ProjectLibrary.configs;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ua.karazin.interfaces.ProjectLibrary.services.CompositeUserDetailsService;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -19,11 +26,13 @@ import ua.karazin.interfaces.ProjectLibrary.services.CompositeUserDetailsService
 public class SecurityConfig {
 
     private final CompositeUserDetailsService compositeUserDetailsService;
+    private final JWTFilter jwtFilter;
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
@@ -35,30 +44,39 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authenticationProvider(authenticationProvider())
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                                .anyRequest().permitAll() // TODO: temporally
-                        /*.requestMatchers("/book/search**", "/book/info**"*//*novelties*//*).permitAll()
-                        .requestMatchers("/book-copy/get-readers-books**"*//*<-hasAnyRole*//*, "book-reservation/reserve-book**", "reader/info**"*//*<-hasAnyRole*//*).hasRole("READER")
+                        .requestMatchers("/auth/login**", "/book/search**", "/book/info**"/*novelties*/).permitAll()
+                        .requestMatchers("/book-copy/get-readers-books**"/*<-hasAnyRole*/, "book-reservation/reserve-book**", "reader/info**"/*<-hasAnyRole*/).hasRole("READER")
                         .requestMatchers("/book/add-book**", "/book-copy/add-book-copies**", "/book-copy/delete-book-copy", "/book-copy/assign-book-copy", "/book-copy/release-book-copy**", "/reader/search**").hasRole("LIBRARIAN")
 
+                        //TODO: .requestMatchers("/auth/registration/reader**", "/auth/registration/librarian**").hasRole("ADMIN")
 
-                        .anyRequest().hasAnyRole("READER", "LIBRARIAN"*//*, "ADMIN"*//*)// Require authentication for all other requests*/
+                        .anyRequest().hasAnyRole("READER", "LIBRARIAN"/*, "ADMIN"*/)// Require authentication for all other requests
                 )
-                .formLogin(form -> form
+                /*.formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error")
                         .permitAll() // Allow access to any user for default login page
-                )
+                )*/
+                .rememberMe(rememberMe -> rememberMe.userDetailsService(compositeUserDetailsService))
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/auth/login?logout=true")
-                        .deleteCookies("JSESSIONID")
-                        .invalidateHttpSession(true)
-                        .permitAll()
-                );
+                        .logoutSuccessUrl("/auth/login")
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ src/main/java/ua/karazin/interfaces/ProjectLibrary/dto/AuthenticationDTO.java
@@ -0,0 +1,7 @@
+package ua.karazin.interfaces.ProjectLibrary.dto;
+
+public record AuthenticationDTO(
+        String username,
+        String password
+) {
+}
