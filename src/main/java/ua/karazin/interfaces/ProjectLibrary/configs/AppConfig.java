17f8b0059f743f99fd4d7d0ea800045f768a6e1d
Artem_Ziablov <artem.ziablov13@gmail.com>
2024-05-14 18:31:04 +0200

    test commit

--- src/main/java/ua/karazin/interfaces/ProjectLibrary/configs/AppConfig.java
+++ src/main/java/ua/karazin/interfaces/ProjectLibrary/configs/AppConfig.java
@@ -3,6 +3,7 @@ package ua.karazin.interfaces.ProjectLibrary.configs;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+// hello
 @EnableConfigurationProperties({BookProperties.class, StatisticsProperties.class})
 @Configuration
 public class AppConfig {
