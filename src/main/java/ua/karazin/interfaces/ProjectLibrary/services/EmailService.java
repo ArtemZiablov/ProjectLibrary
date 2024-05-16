ae4100e1e8cc01e62456ac6647c870702be25ab1
Artem_Ziablov <artem.ziablov13@gmail.com>
2024-05-16 13:30:05 +0200

    add email sending

---
+++ src/main/java/ua/karazin/interfaces/ProjectLibrary/services/EmailService.java
@@ -0,0 +1,26 @@
+package ua.karazin.interfaces.ProjectLibrary.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailService {
+
+
+    private final JavaMailSender mailSender;
+
+    @PostMapping("/send")
+    public void sendSimpleEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
